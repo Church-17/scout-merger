60f4460442745eb288be5e5c0adc74226bb1e65b
Church-17 <matteo.chiesa.mc@gmail.com>
2024-02-27 21:35:39 +0100

    type

--- res.py
+++ res.py
@@ -1,12 +1,12 @@
 from tempfile import gettempdir
 
 title: str = "ScoutMerger"
-author = "Matteo Chiesa"
+author: str = "Matteo Chiesa"
 version: str = "1.0"
 
-spreadsheet_url = "https://docs.google.com/spreadsheets/d/1prvCn3L06m3A7_31DEKfzN9CowLZK_2Gu-A6Q3VI-TY"
+spreadsheet_url: str = "https://docs.google.com/spreadsheets/d/1prvCn3L06m3A7_31DEKfzN9CowLZK_2Gu-A6Q3VI-TY"
 sheet_name: str = "Località"
-repo_url = "https://github.com/Church-17/scout_merger"
+repo_url: str = "https://github.com/Church-17/scout_merger"
 
 ftypes: dict[str, tuple[str, str]] = {
     "Word": ("Word document","*.docx"),
