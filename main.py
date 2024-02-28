d1c4277c201649bf77b48771aa2c594b7b532dd1
Church-17 <matteo.chiesa.mc@gmail.com>
2024-02-28 10:11:34 +0100

    better tk import

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from os import remove, path
-from threading import Thread
 from webbrowser import open as browse
-import tkinter as tk
+from tkinter import constants as tk_const, filedialog, Tk, Menu
 from tkinter.ttk import *
-from tkinter import Menu, filedialog
 from mailmerge import MailMerge
 from docx2pdf import convert as docx2pdf
 
+# from tk import *
 from sheet import GoogleSheet
 from res import title, spreadsheet_url, sheet_name, ftypes, temp_path, settings_path
 from top_window import view_credits, showinfo, showerror
@@ -38,7 +37,7 @@ class ScoutMergerGUI:
         self.loc_info = self.loc_info.dropna(subset=self.header[0]).sort_values(self.header[0]).fillna("")
 
     def setup_window(self):
-        self.window = tk.Tk()
+        self.window = Tk()
         self.window.geometry("700x400")
         self.window.minsize(700, 400)
         self.window.title(title)
@@ -62,53 +61,54 @@ class ScoutMergerGUI:
         self.window.config(menu=self.menu_bar)
 
         self.loc_frame = Frame(self.window)
-        self.loc_frame.grid(row=0, column=0, columnspan=2, pady=8, sticky=tk.N)
+        self.loc_frame.grid(row=0, column=0, columnspan=2, pady=8, sticky=tk_const.N)
         self.loc_label = Label(self.loc_frame, text="Località:")
         self.loc_label.grid(row=0, column=0, padx=5)
         self.loc_combo = Combobox(self.loc_frame, values=list(self.loc_info[self.header[0]]), width=30)
         self.loc_combo.grid(row=0, column=1, padx=5)
+        # self.loc_combo.bind('<KeyRelease>', self.check_input)
         self.loc_combo.bind("<<ComboboxSelected>>", self.update_recap)
         
         self.recap_frame = Frame(self.window)
-        self.recap_frame.grid(row=1, column=0, rowspan=2, padx=10, pady=8, sticky=tk.NW)
+        self.recap_frame.grid(row=1, column=0, rowspan=2, padx=10, pady=8, sticky=tk_const.NW)
         self.recap_frame.columnconfigure(1, weight=1)
         self.recap_frame.rowconfigure(0, weight=1)
         self.header_frame = Frame(self.recap_frame)
-        self.header_frame.grid(row=0, column=0, sticky=tk.NW)
+        self.header_frame.grid(row=0, column=0, sticky=tk_const.NW)
         self.data_frame = Frame(self.recap_frame)
-        self.data_frame.grid(row=0, column=1, sticky=tk.NW)
+        self.data_frame.grid(row=0, column=1, sticky=tk_const.NW)
         self.recap_labels = [Label(self.header_frame, text=f"{head}:") for head in self.header]
         self.recap_values = [Label(self.data_frame, text="") for _ in range(len(self.header))]
         for row in range(len(self.header)):
             self.recap_labels[row].grid(row=row, column=0, padx=5)
-            self.recap_values[row].grid(row=row, column=0, padx=5, sticky=tk.W)
+            self.recap_values[row].grid(row=row, column=0, padx=5, sticky=tk_const.W)
         
         self.model_frame = Frame(self.window)
-        self.model_frame.grid(row=1, column=1, padx=10, pady=8, sticky=tk.NE)
+        self.model_frame.grid(row=1, column=1, padx=10, pady=8, sticky=tk_const.NE)
         self.model_frame.columnconfigure(0, minsize=110)
         self.model_frame.columnconfigure(1, minsize=140)
         self.model_label = Label(self.model_frame, text="Modello:")
         self.model_label.grid(row=0, column=0, padx=5)
         self.upload_button = Button(self.model_frame, text="Carica", command=self.upload_model)
-        self.upload_button.grid(row=0, column=1, padx=5, pady=3, sticky=tk.W)
+        self.upload_button.grid(row=0, column=1, padx=5, pady=3, sticky=tk_const.W)
         self.model_name = Label(self.model_frame)
         self.model_name.grid(row=1, column=0, columnspan=2, padx=5, pady=3)
 
         self.notify_frame = Frame(self.window)
-        self.notify_frame.grid(row=2, column=0, padx=20, pady=10, sticky=tk.SW)
+        self.notify_frame.grid(row=2, column=0, padx=20, pady=10, sticky=tk_const.SW)
         self.notify_label = Label(self.notify_frame, font=("Segoe UI", 12))
         self.notify_label.grid(row=0, column=0, pady=5)
 
         self.button_frame = Frame(self.window)
-        self.button_frame.grid(row=2, column=1, padx=10, pady=12, sticky=tk.SE)
-        self.save_button = Button(self.button_frame, text="Salva", state=tk.DISABLED, command=self.save)
+        self.button_frame.grid(row=2, column=1, padx=10, pady=12, sticky=tk_const.SE)
+        self.save_button = Button(self.button_frame, text="Salva", state=tk_const.DISABLED, command=self.save)
         self.save_button.grid(row=0, column=0, padx=5)
         self.close_button = Button(self.button_frame, text="Chiudi", command=self.window.destroy)
         self.close_button.grid(row=0, column=1, padx=5)
 
     def try_enable_save(self):
         if self.loc_picked and self.model_picked:
-            self.save_button.config(state=tk.ACTIVE)
+            self.save_button.config(state=tk_const.ACTIVE)
 
     def update_db(self):
         self.notify_label.config(text="Aggiornamento database...", foreground="blue")
@@ -140,10 +140,15 @@ class ScoutMergerGUI:
             return
         self.notify_label.config(text="Dowload completato", foreground="green")
 
+    def check_input(self, event):
+        self.loc_combo.config(values=list(self.loc_info[self.loc_info[self.header[0]].str.lower().str.contains(self.loc_combo.get().lower())][self.header[0]]))
+        self.loc_combo.event_generate('<Down>')
+        self.window.after(1, self.loc_combo.winfo_children)
+
     def update_recap(self, event):
         if self.loc_combo.current() < 0:
             return
-        self.loc_dict = self.loc_info.loc[self.loc_info[self.header[0]] == self.loc_combo.get()].iloc[0]
+        self.loc_dict = self.loc_info[self.loc_info[self.header[0]] == self.loc_combo.get()].iloc[0]
         for value, data in zip(self.recap_values, self.loc_dict.values):
             if data == True:
                 data = "\U00002705"
--- top_window.py
+++ top_window.py
@@ -1,8 +1,8 @@
 from webbrowser import open as browse
-import tkinter as tk
+from tkinter import constants as tk_const, messagebox, Tk, Toplevel
 from tkinter.ttk import *
-from tkinter import messagebox
 
+# from tk import *
 from res import title, version, author, spreadsheet_url, repo_url
 
 infobox: dict[str, str] = {
@@ -20,8 +20,8 @@ def showinfo(info):
 def showerror(err):
     messagebox.showerror(title, errorbox[err])
 
-def view_credits(parent_win: tk.Tk):
-    top_win = tk.Toplevel(parent_win)
+def view_credits(parent_win: Tk):
+    top_win = Toplevel(parent_win)
     top_win.geometry("200x200")
     top_win.resizable(False, False)
     top_win.title(title)
@@ -29,21 +29,21 @@ def view_credits(parent_win: tk.Tk):
     top_win.columnconfigure(0, weight=1)
 
     info_label = Label(top_win, text="Informazioni", font=("Segoe UI", 14))
-    info_label.grid(row=0, column=0, padx=10, pady=5, sticky=tk.W)
+    info_label.grid(row=0, column=0, padx=10, pady=5, sticky=tk_const.W)
     version_label = Label(top_win, text=f"{title}")
-    version_label.grid(row=1, column=0, padx=20, sticky=tk.W)
+    version_label.grid(row=1, column=0, padx=20, sticky=tk_const.W)
     version_label = Label(top_win, text=f"Versione {version}")
-    version_label.grid(row=2, column=0, padx=20, sticky=tk.W)
+    version_label.grid(row=2, column=0, padx=20, sticky=tk_const.W)
     author_label = Label(top_win, text=f"Creato da {author}")
-    author_label.grid(row=3, column=0, padx=20, sticky=tk.W)
+    author_label.grid(row=3, column=0, padx=20, sticky=tk_const.W)
     db_label = Label(top_win, text=f"Database", font=("TkDefaultFont", 9, "underline"), foreground="blue", cursor="hand2")
-    db_label.grid(row=4, column=0, padx=20, pady=(10, 0), sticky=tk.W)
+    db_label.grid(row=4, column=0, padx=20, pady=(10, 0), sticky=tk_const.W)
     db_label.bind("<Button-1>", lambda e: browse(spreadsheet_url))
     repo_label = Label(top_win, text=f"Codice sorgente", font=("TkDefaultFont", 9, "underline"), foreground="blue", cursor="hand2")
-    repo_label.grid(row=5, column=0, padx=20, sticky=tk.W)
+    repo_label.grid(row=5, column=0, padx=20, sticky=tk_const.W)
     repo_label.bind("<Button-1>", lambda e: browse(repo_url))
 
     ok_button = Button(top_win, text="Ok", command=top_win.destroy)
-    ok_button.grid(row=6, column=0, pady=5, sticky=tk.S)
+    ok_button.grid(row=6, column=0, pady=5, sticky=tk_const.S)
 
     top_win.update()
